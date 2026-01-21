bd5991b3b2a72ad93064ea6b0597d47daaa7a3ea
Ninjed <jedrickschool@gmail.com>
2026-01-21 14:43:23 +0800

    volley imported

---
+++ app/src/main/java/com/example/brewtiful_cafe/Constants.java
@@ -0,0 +1,5 @@
+package com.example.brewtiful_cafe;
+
+public class Constants {
+    public static final String register_url = "http://10.0.2.2/";
+}
--- app/src/main/java/com/example/brewtiful_cafe/register_screen.java
+++ app/src/main/java/com/example/brewtiful_cafe/register_screen.java
@@ -7,8 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 public class register_screen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
